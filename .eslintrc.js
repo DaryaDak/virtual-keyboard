445e28c4e5276a9d7d3f7cefffc695743abc0385
Admin <admin@iMac-Admin.local>
2022-05-10 18:56:06 +0300

    fix: fix eslint.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "airbnb-base",
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
 
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    
-    "rules": {
-        "no-param-reassign": 0,
-    }
-}
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+
+  rules: {
+    'no-param-reassign': 0,
+  },
+};
